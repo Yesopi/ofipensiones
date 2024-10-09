800d2b79b98efbde56ccd950d5e83472cd2ddf86
Yesopi <y.pineros@uniandes.edu.co>
2024-10-08 23:19:58 -0500

    ya no es solo un readme

---
+++ ofipensionesOrigin/ofipensiones/manejador_pago/urls.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ManejadorInstitucionesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'manejador_instituciones'
---
+++ ofipensionesOrigin/ofipensiones/manejador_recibos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ManejadorRecibosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'manejador_recibos'
